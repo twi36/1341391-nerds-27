660e6bcd4e0fc1015a643e0eeb92778a912feba4
twi36 <twi27light@yandex.ru>
2020-03-14 07:41:21 +0300

    Правки

--- js/popup.js
+++ js/popup.js
@@ -5,32 +5,19 @@ var form = popup.querySelector("form");
 var username = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
 
-var isStorageSupport = true;
-var storage = "";
-
-try {
-    storage = localStorage.getItem("username");
-} catch (err) {
-    isStorageSupport = false;
-}
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("write-modal-show");
-
-    if (storage) {
-        username.value = storage;
-        email.focus();
-    } else {
-        username.focus();
-    }
+    username.focus();
 });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.remove("write-modal-show");
+    popup.classList.add("write-modal-close");
+    //popup.classList.remove("write-modal-show");
     popup.classList.remove("write-modal-error");
-    //popup.classList.add("write-modal-close");
+    
 });
 
 form.addEventListener("submit", function (evt) {
@@ -39,10 +26,6 @@ form.addEventListener("submit", function (evt) {
         popup.classList.remove("write-modal-error");
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("write-modal-error");
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("username", username.value);
-        }
     }
 });
 
@@ -50,9 +33,9 @@ window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
         if (popup.classList.contains("write-modal-show")) {
-            popup.classList.remove("write-modal-show");
-            popup.classList.remove("write-modal-error");
-            //popup.classList.add("write-modal-close");
+                popup.classList.add("write-modal-close");
+                //popup.classList.remove("write-modal-show");
+                popup.classList.remove("write-modal-error");
         }
     }
 });
